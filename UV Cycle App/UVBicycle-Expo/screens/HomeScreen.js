8eb3a9877d574c63a7c0f886cd6a5c8a3af3985e
zihanmo <mozihan620@gmail.com>
2019-08-27 23:57:00 +1000

    add weather forecast page

--- UV Cycle App/UVBicycle-Expo/screens/HomeScreen.js
+++ UV Cycle App/UVBicycle-Expo/screens/HomeScreen.js
@@ -1,5 +1,10 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import PropTypes from 'prop-types';
+
+
 import {
   Image,
   Platform,
@@ -12,66 +17,84 @@ import {
 
 import { MonoText } from '../components/StyledText';
 
-export default function HomeScreen() {
+
+export default function HomeScreen({image1, time2,time3, time4,time5, time6,weather1, temperature,location,temperature2,weather2, temperature3,weather3, temperature4,weather4, temperature5,weather5,temperature6,weather6,}) {
+
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+    
+    
+    
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
 
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/ConfigurationScreen.js</MonoText>
-          </View>
+        <View style={styles.weatherContainer}>
+      <View style={styles.headerContainer}>
+      <Text style={styles.subtitle}>Weather Forecast</Text>
+  
 
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
 
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView >
+   
+      {WeatherDescToImageSource(weather1)}
 
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
+      
 
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
+
+    
+
+      <Text style={styles.tempText11}>{temperature} C</Text>
+      <Text style={styles.tempText1}>{weather1}</Text>
       </View>
+
+      
+      <View style={styles.BodyContainer}>
+
+      <View style={styles.weather1con}>
+      <Text style={styles.tempText}>{time2}</Text>
+      {WeatherDescToSmallImageSource(weather2)}
+      <Text style={styles.tempText}>{temperature2} C</Text>
+    
+      </View>
+
+      <View style={styles.weather1con}>
+      <Text style={styles.tempText}>{time3}</Text>
+      {WeatherDescToSmallImageSource(weather3)}
+      <Text style={styles.tempText}>{temperature3} C</Text>
+ 
+      </View>
+
+      <View style={styles.weather1con}>
+      <Text style={styles.tempText}>{time4}</Text>
+      {WeatherDescToSmallImageSource(weather4)}
+      <Text style={styles.tempText}>{temperature4} C</Text>
+
+      </View>
+
+      <View style={styles.weather1con}>
+      <Text style={styles.tempText}>{time5}</Text>
+      {WeatherDescToSmallImageSource(weather5)}
+      <Text style={styles.tempText}>{temperature5} C</Text>
+   
+      </View>
+
+      <View style={styles.weather1con}>
+      <Text style={styles.tempText}>{time6}</Text>
+      {WeatherDescToSmallImageSource(weather6)}
+      <Text style={styles.tempText}>{temperature6} C</Text>
+   
+      </View>
+      </View>
+      
+    
     </View>
+   
+     
   );
 }
 
 HomeScreen.navigationOptions = {
   header: null,
 };
+
 
 function DevelopmentModeNotice() {
   if (__DEV__) {
@@ -121,20 +144,46 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   contentContainer: {
-    paddingTop: 30,
+    marginTop: 30,
   },
+  heardercontainer: {
+    paddingTop: 40,
+  },
+ 
   welcomeContainer: {
     alignItems: 'center',
-    marginTop: 10,
+    marginTop: 30,
     marginBottom: 20,
   },
+
+  forcastContainer: {
+
+    alignItems: 'center',
+  },
   welcomeImage: {
-    width: 100,
-    height: 80,
+    width: 170,
+    height: 170,
     resizeMode: 'contain',
     marginTop: 3,
-    marginLeft: -10,
+
   },
+
+  welcomeImage2: {
+    width: 60,
+    height: 50,
+    resizeMode: 'contain',
+    marginTop: 13,
+    marginBottom: 30,
+   
+  },
+
+
+  forcastImage: {
+    width: 350,
+    height: 200,
+
+  },
+
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
@@ -151,9 +200,25 @@ const styles = StyleSheet.create({
     paddingHorizontal: 4,
   },
   getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
+    fontSize: 30,
+    color: 'black',
+    lineHeight: 50,
+    textAlign: 'center',
+  },
+
+
+  tempText: {
+    fontSize: 13,
+    color: '#fff',
+    marginTop:17,
+
+  },
+
+
+  weathertext: {
+    fontSize: 20,
+    color: 'blue',
+    lineHeight: 50,
     textAlign: 'center',
   },
   tabBarInfoContainer: {
@@ -195,4 +260,261 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#2e78b7',
   },
+  weatherContainer: {
+   
+
+    backgroundColor: '#ffffff',
+   
+  },
+  headerContainer: {
+
+    alignItems: 'center',
+  
+  },
+ 
+  BodyContainer: {
+    marginTop:30,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    flexDirection: 'row'
+  },
+  title: {
+    marginTop:17,
+    fontSize: 13,
+    color: '#6495ed'
+  },
+  tempText1: {
+    fontSize: 24,
+    color: '#6495ed',
+    marginTop:7
+  },
+
+  tempText11: {
+    fontSize: 44,
+    color: '#6495ed',
+    marginTop:7,
+    fontWeight: 'bold',
+  },
+
+  weather1con:{
+
+  width:75,
+  backgroundColor:'#000',
+  height:190,
+    alignItems: 'center',
+    borderColor: '#000'
+  },
+  
+  subtitle: {
+    marginTop:60,
+    marginBottom:20,
+    fontSize: 33,
+    color: '#000'
+    
+  },
+  imageweather2:{
+    width: 10, 
+    height: 50
+  },
+
+
+
 });
+
+
+
+  
+
+function WeatherDescToImageSource(weatherDesc) {
+  
+  switch (weatherDesc) {
+    case "partly-cloudy-day":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/partly-cloudy-day.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "clear-day":
+      return    <Image style={styles.imageweather2}
+      source={
+        require('../assets/images/clear-day.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "partly-cloudy-night":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/partly-cloudy-night.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "partly-cloudy-day":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/partly-cloudy-day.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "cloudy":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/cloudy.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "rain":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/rain.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "sleet":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/sleet.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "snow":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/snow.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "wind":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/wind.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+    case "fog":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/fog.png')
+
+      }
+      style={styles.welcomeImage}
+    />;
+
+
+
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function WeatherDescToSmallImageSource(weatherDesc1) {
+  
+  switch (weatherDesc1) {
+    case "partly-cloudy-day":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/partly-cloudy-day1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "clear-day":
+      return    <Image style={styles.imageweather2}
+      source={
+        require('../assets/images/clear-day1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "partly-cloudy-night":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/partly-cloudy-night1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "partly-cloudy-day":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/partly-cloudy-day1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "cloudy":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/cloudy1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "rain":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/rain1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "sleet":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/sleet1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "snow":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/snow1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "wind":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/wind1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+    case "fog":
+      return    <Image style={styles.imageweather}
+      source={
+        require('../assets/images/fog1.png')
+
+      }
+      style={styles.welcomeImage2}
+    />;
+
+
+
+
+
+  }
+}
