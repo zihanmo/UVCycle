35e17b9b277a2f58ad76fe2acbd2f68be6149be9
s4446540 <yian.chen1@uqconnect.edu.au>
2019-10-08 22:44:55 +1000

    all about plots

---
+++ InfoWeb/plots/plotdata.php
@@ -0,0 +1,15 @@
+<?php
+require 'database.php';
+
+$db = new MySQLDatabase();
+$db->connect();
+$query = "SELECT * FROM Clicks";
+$result = $db->query($query);
+$arr = array();
+if ($result->num_rows > 0) {
+	while ($row = $result->fetch_assoc()) {
+        $arr[$row['calendar']] = $row['clicks'];
+    }
+}
+echo(json_encode($arr));
+?>
