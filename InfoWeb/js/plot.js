b25439329fca83550965a58b3caef479bff63dc7
Weizhou Ren <weizhou.ren@uqconnect.edu.au>
2019-10-09 21:39:17 +1000

    Access data

--- InfoWeb/js/plot.js
+++ InfoWeb/js/plot.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var ajax = new XMLHttpRequest();
     var method = "GET";
-    var url = "plotdata.php";
+    var url = "../plotdata.php";
     var asynchronous = true;
     ajax.open(method, url, asynchronous);
     ajax.send();
--- InfoWeb/plotdata.php
+++ InfoWeb/plotdata.php
@@ -3,12 +3,12 @@ require 'connectDB.php';
 
 $db = new MySQLDatabase();
 $db->connect();
-$query = "SELECT * FROM Clicks";
+$query = "SELECT purchase_date, COUNT(*) FROM Users GROUP BY purchase_date";
 $result = $db->query($query);
 $arr = array();
 if ($result->num_rows > 0) {
 	while ($row = $result->fetch_assoc()) {
-        $arr[$row['calendar']] = $row['clicks'];
+        $arr[$row['purchase_date']] = $row['clicks'];
     }
 }
 echo(json_encode($arr));
