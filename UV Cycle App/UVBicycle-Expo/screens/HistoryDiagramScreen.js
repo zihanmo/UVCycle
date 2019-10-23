5d14e3d19e98b62eed3ce5719ea265382d491614
Weizhou Ren <s4493996@student.uq.edu.au>
2019-10-23 23:32:28 +1000

    Fixed x axis in UV history diagram

--- UV Cycle App/UVBicycle-Expo/screens/HistoryDiagramScreen.js
+++ UV Cycle App/UVBicycle-Expo/screens/HistoryDiagramScreen.js
@@ -16,6 +16,9 @@ import {
   Dimensions,
   ImageBackground,
   AsyncStorage,
+  TouchableOpacity,
+  Image,
+  Platform
 } from 'react-native';
 import {
   Circle
@@ -23,7 +26,7 @@ import {
 import * as shape from 'd3-shape';
 
 export default class HistoryDiagramScreen extends Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
@@ -39,7 +42,7 @@ export default class HistoryDiagramScreen extends Component {
    */
   getindex = () => {
     AsyncStorage.multiGet(["email", "date"]).then(res => {
-      var data = {email: res[0][1], date: res[1][1]};
+      var data = { email: res[0][1], date: res[1][1] };
       fetch('http://deco3801-teamwyzards.uqcloud.net/uvhistory.php', {
         method: 'POST',
         headers: {
@@ -48,19 +51,20 @@ export default class HistoryDiagramScreen extends Component {
         },
         body: JSON.stringify(data)
       })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        var timetemp = [];
-        var indexes = [];
-        for (t in responseJson) {
-          timetemp.push(t);
-          indexes.push(responseJson[t]);
-        }
-        this.setState({
-          time: timetemp,
-          index: indexes
+        .then((response) => response.json())
+        .then((responseJson) => {
+          var timetemp = [];
+          var indexes = [];
+          for (t in responseJson) {
+            timetemp.push(t);
+            indexes.push(responseJson[t]);
+          }
+          this.setState({
+            time: timetemp,
+            index: indexes
+          })
         })
-      })
+        .catch((error) => console.error(error));
     })
   }
 
@@ -70,13 +74,13 @@ export default class HistoryDiagramScreen extends Component {
       y,
       data
     }) => {
-      return data.map((value, index) => ( <
-        Circle key = {index}
-        cx = {x(index)}
-        cy = {y(value)}
-        r = {1.2}
-        stroke = {'rgb(134, 65, 244)'}
-        fill = {'black'}/>
+      return data.map((value, index) => (<
+        Circle key={index}
+        cx={x(index)}
+        cy={y(value)}
+        r={1.2}
+        stroke={'rgb(134, 65, 244)'}
+        fill={'black'} />
       ));
     }
 
@@ -97,50 +101,70 @@ export default class HistoryDiagramScreen extends Component {
 
     return (
       <ScrollView>
+        <TouchableOpacity style={styles.uvButton} onPress={() => navigate("Main")}>
+          <Image style={styles.backPic} source={require('../assets/images/back.png')} />
+        </TouchableOpacity>
         <View style={styles.titleContainer}>
-          <Text style={styles.title}>UV history index</Text>
+          <Text style={styles.title}>UV History Index</Text>
         </View>
         <View style={{ height: Dimensions.get('window').height - 200, padding: 20, flexDirection: 'row' }}>
           <YAxis
-            data={ [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] }
+            data={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
             //yAccessor={ ({ item }) => item.index }
             //style={{ marginBottom: xAxisHeight }}
             contentInset={verticalContentInset}
-            svg={axesSvg}/>
+            svg={axesSvg} />
           <View style={{ flex: 1, marginLeft: 10 }}>
-            <ImageBackground source={require('../assets/images/indexback.png')} style={{width: '100%', height: '100%'}}>
+            <ImageBackground source={require('../assets/images/indexback.png')} style={{ width: '100%', height: '100%' }}>
               <LineChart
                 style={{ flex: 1 }}
-                data={ indexes }
+                data={indexes}
                 //contentInset={{ top: 30, bottom: 30 }}
                 curve={shape.curveNatural}
                 contentInset={verticalContentInset}
                 svg={{ stroke: 'rgb(0, 0, 0)', strokeWidth: 3 }}
                 yMax={12}
                 yMin={0}>
-                <Grid/>
-                <Decorator/>
+                <Grid />
+                <Decorator />
               </LineChart>
             </ImageBackground>
-            <XAxis
+            {/* <XAxis
               style={{ marginHorizontal: -10, height: xAxisHeight, marginTop: 10 }}
               data={data}
-              xAccessor={ ({ item }) => item.time }
-              numberOfTicks={ data.length }
+              xAccessor={({ item }) => item.time}
+              numberOfTicks={data.length}
               // format improvement
-              formatLabel={ value => `22:${value}` }
+              // formatLabel={value => `22:${value}`}
               contentInset={{ left: 10, right: 10 }}
-              svg={axesSvg}/>
+              svg={axesSvg} /> */}
           </View>
         </View>
-        </ScrollView>
+      </ScrollView>
     )
   }
 }
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50
+  },
+  desc: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10,
+    margin: 15,
+    borderRadius: 15,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 0, height: 1 },
+        shadowColor: 'grey',
+        shadowOpacity: 0.2,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
   },
   contentContainer: {
     paddingTop: 30,
@@ -149,11 +173,16 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   title: {
-    paddingTop:40,
-    paddingBottom:20,
+    paddingTop: 40,
+    paddingBottom: 20,
     fontSize: 33,
     color: '#1E6738',
     alignSelf: 'center'
+  },
+  backPic: {
+    height: 30,
+    width: 25,
+    marginLeft: 15,
   },
 });
 
