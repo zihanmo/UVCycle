8646e69b6e1ff0c74a4ae6d441e26e817c5672f5
William Meehan <william.meehan@uq.net.au>
2019-10-23 20:12:59 +1000

    Implemented storage of UV sensor readings in database via WIFI.

--- Backend/updateUVIndex.php
+++ Backend/updateUVIndex.php
@@ -7,9 +7,9 @@ $data = json_decode(file_get_contents('php://input'), true);
 $db = new MySQLDatabase();
 $db->connect();
 $uvindex = $data["uvindex"];
-$userid = $data["userid"];
+$sensorid = $data["sensorid"];
 $history_id = $data["history_id"];
-$query = "REPLACE INTO History (uvindex, userid, history_id) VALUES ('$uvindex', '$userid', '$history_id')"; 
+$query = "REPLACE INTO History (uvindex, sensorid, history_id) VALUES ('$uvindex', '$sensorid', '$history_id')"; 
 $result = $db->query($query);
 
 $db->disconnect();
--- UV Cycle App/Sensor/uv_cycle.ino
+++ UV Cycle App/Sensor/uv_cycle.ino
@@ -7,10 +7,17 @@
 #include "WiFi.h" // ESP32 WiFi
 #include <stdlib.h>
 
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+uint8_t txValue = 0;
 WiFiClient client;
+
+RTC_DATA_ATTR int bootCount = -1;
 
 String data;
 int historyid;
+volatile int interruptCounter = 0;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -22,7 +29,7 @@ int historyid;
 
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define LED_PIN     15
+#define LED_PIN     12
 
 // How many NeoPixels are attached to the Arduino?
 #define LED_COUNT  16
@@ -30,8 +37,10 @@ int historyid;
 // NeoPixel brightness, 0 (min) to 255 (max)
 #define BRIGHTNESS 50
 
+#define SENSOR_ID 13
+
 // Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGBW + NEO_KHZ800);
 // Argument 1 = Number of pixels in NeoPixel strip
 // Argument 2 = Arduino pin number (most are valid)
 // Argument 3 = Pixel type flags, add together as needed:
@@ -42,10 +51,10 @@ Adafruit_SI1145 uv = Adafruit_SI1145();
 
 void ConnectToWiFi()
 {
-
-  //enter wifi username and password here
+ 
   WiFi.mode(WIFI_STA);
-  WiFi.begin("", "");
+  WiFi.begin("AndroidAPD987", "ongp1202");
+  Serial.print("Connecting to "); Serial.println("AndroidAPD987");
  
   uint8_t i = 0;
   while (WiFi.status() != WL_CONNECTED)
@@ -63,39 +72,90 @@ void ConnectToWiFi()
   Serial.println(WiFi.localIP());
 }
 
+void IRAM_ATTR blink() {
+    esp_deep_sleep_start();
+}
 
+/*
+Method to print the reason by which ESP32
+has been awaken from sleep
+*/
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+int loopCount = 0;
 void setup() {
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(BRIGHTNESS); // Set BRIGHTNESS to about 1/5 (max = 255)
 
-  Serial.begin(115200);
-  ConnectToWiFi();
+    Serial.begin(115200);
 
+    //Increment boot number and print it every reboot
+  ++bootCount;
+  loopCount = 0;
+
+  Serial.println("Boot number: " + String(bootCount));
+    
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_13, 1);
+    pinMode(13, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(13), blink, FALLING);
+
+      if (bootCount == 1) {
+    esp_deep_sleep_start();
+  }
+
+    ConnectToWiFi();
+
+  
+  Serial.println("Waiting a client connection to notify...");
+  
   Serial.println("Adafruit SI1145 test");
   
   if (! uv.begin()) {
     Serial.println("Didn't find Si1145");
     while (1);
   }
-  
+
+  //Serial.println("OK!");
   data = "";
   historyid = 1;
 
 }
 
 uint32_t currentRingColour = 0;
-
+int deepSleep = 0;
 void loop() {
+
+
+  Serial.println("interrupt count");
+  Serial.println(interruptCounter);
   Serial.println("===================");
   Serial.print("Vis: "); Serial.println(uv.readVisible());
   Serial.print("IR: "); Serial.println(uv.readIR());
+  
+  // Uncomment if you have an IR LED attached to LED pin!
+  //Serial.print("Prox: "); Serial.println(uv.readProx());
 
   float UVindex = uv.readUV();
+  // the index is multiplied by 100 so to get the
+  // integer index, divide by 100!
   UVindex /= 1.0;  
   Serial.print("UV: ");  Serial.println(UVindex);
 
-  //Set NeoPixel ring colour according to uv index
+  
   switch ((int)UVindex) {
     case 0:
     case 1:
@@ -120,14 +180,39 @@ void loop() {
       setLEDRingColour(238, 130, 238); // Violet
   } 
 
-  // Latest uv index should be stored at index 0 of history table.
-  char buffer[50]; 
-  sprintf(buffer, "{\"historyid\": \"0\", \"uvindex\": %f}", UVindex); 
+      
+     if (deviceConnected) {
+        txValue = UVindex;
+  }
+
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+  
+
+  //data = "historyid=" + historyid + "&uvindex=" + (int)UVindex;
+  data = "historyid=20&uvindex=19";
+  uint8_t uvlive = (int)UVindex;
+    char buffer2[20];
+    sprintf(buffer2, "%d", uvlive);
+    Serial.print("sprintf");
+    Serial.print(buffer2);
+  
+  char buffer[100]; 
+  sprintf(buffer, "{\"uvindex\": %f, \"sensorid\": %d, \"history_id\": %d}", UVindex, SENSOR_ID, bootCount); 
   Serial.print(buffer);
+  Serial.print(buffer2);
 
   
-  // Post uv index to History table of database
-  if (client.connect("deco3801-teamwyzards.uqcloud.net",80)) { // REPLACE WITH YOUR SERVER ADDRESS
+
+  if (client.connect("deco3801-teamwyzards.uqcloud.net",80) && (loopCount % 5 == 0)) { // REPLACE WITH YOUR SERVER ADDRESS
     client.println("POST /updateUVIndex.php HTTP/1.1"); 
     client.println("Host: deco3801-teamwyzards.uqcloud.net"); // SERVER ADDRESS HERE TOO
     client.println("Content-Type: application/x-www-form-urlencoded"); 
@@ -135,21 +220,20 @@ void loop() {
     client.println(strlen(buffer)); 
     client.println(); 
     client.print(buffer); 
-
+    historyid = historyid + 1;
     Serial.print("connected");
   } else {
     Serial.print("didn't connect");
   }
 
   if (client.connected()) { 
-    client.stop();
+    client.stop();  // DISCONNECT FROM THE SERVER
   }
 
-
+  loopCount = loopCount + 1;
   delay(1000);
 }
 
-// Helper function to set Neopixel ring colour
 void setLEDRingColour(int r, int g, int b) {
   if (currentRingColour != strip.Color(r, g, b)) {
     strip.fill(strip.Color(r, g, b));
